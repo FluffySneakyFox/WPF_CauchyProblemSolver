0c78f9bd7f298e93b7da0ec5ef55ddd3d3c4ab76
FluffySneakyFox <sergey_belayev@inbox.ru>
2022-06-04 00:13:53 +0300

    Added graphics

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPF_CauchyProblemSolver.Classes;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace WPF_CauchyProblemSolver
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,16 +13,58 @@ namespace WPF_CauchyProblemSolver
             InitializeComponent();
         }
 
+        private ICalculator GetCalculator()
+        {
+            ICalculator calc = new Euler();
+            switch (Convert.ToInt32(MethodCB.SelectedIndex))
+            {
+                case 0:
+                    calc = new Euler();
+                    break;
+                default:
+                    _ = MessageBox.Show("Not implemented method!", "Error", MessageBoxButton.OK,MessageBoxImage.Error);
+                    break;
+            }
+            return calc;
+        }
+
+        private void DrawGraph(double start, double step, double[] dots)
+        {
+            //Clear old dots
+           /* if(oxyPlotCanvas.Model.Series != null)
+            {
+                oxyPlotCanvas.Model.Series.Clear();
+            }*/
+
+            PlotModel plotModel = new PlotModel(){Title = "Graph"};
+            LineSeries Series = new LineSeries();
+            for (int i = 0; i < dots.Length; i++)
+            {
+                Series.Points.Add(new DataPoint(start+step*i, dots[i]));
+            }
+            plotModel.Series.Add(Series);
+            oxyPlotCanvas.Model = plotModel;
+        }
+
         private void Calc_Click(object sender, RoutedEventArgs e)
         {
             //Get data from form
+            double
+                start = Convert.ToDouble(StartTB.Text),
+                end = Convert.ToDouble(EndTB.Text),
+                step = Convert.ToDouble(StepTB.Text),
+                f0 = Convert.ToDouble(CauchyTB.Text);
+            ICalculator calc = GetCalculator();
 
+            
+            Func<double, double> testFunc = x => 1/(2*Math.Sqrt(x));
+            //https://github.com/Firestorm01X2/MathDotNetTest/blob/main/MathDotNetTest/Program.cs
 
             //Solve problem
-
+            double[] diffEqDots = calc.Calculate(start, end, step, testFunc, f0);
 
             //Draw graph
-
+            DrawGraph(start,step,diffEqDots);
         }
     }
 }
